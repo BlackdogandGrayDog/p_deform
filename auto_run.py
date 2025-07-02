1c0828df4798224117bca38e5ef75988229e4b5b
BlackdogandGrayDog <ericwei09@outlook.com>
2025-07-02 23:17:02 +0100

    update for trj 2

--- auto_run.py
+++ auto_run.py
@@ -6,13 +6,13 @@ mode = "base" # base or finetune
 
 
 # Define training parameter combinations
-trajectories = ['2']
+trajectories = ['1']
 loss_models = ['patched_0.005_0.01']
-steps = ['500k']
+steps = ['900k']
 
 dataset_name = "simulator"
 finetune_dataset_name = dataset_name
-finetune_trajectory = "2"
+finetune_trajectory = "1"
 
 # Iterate over all parameter combinations
 for trajectory in trajectories:
